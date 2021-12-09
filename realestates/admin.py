46032d270bb16330a22faf6d75c4e5cb1d19617d
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2021-12-09 23:33:36 +0100

    update

--- realestates/admin.py
+++ realestates/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from realestates.models import Address, Plot, Realestate, Flat
+from realestates.models import Address, Plot, Realestate, Flat, Garage
 
 admin.site.register(Address)
 admin.site.register(Plot)
 admin.site.register(Realestate)
 admin.site.register(Flat)
+admin.site.register(Garage)
--- realestates/models.py
+++ realestates/models.py
@@ -8,11 +8,29 @@ class Address(models.Model):
     zip_code = models.CharField(verbose_name='ZIP Code', max_length=10, null=True, blank=False)
     city = models.CharField(verbose_name='City', max_length=100, null=True, blank=False)
 
+    def __str__(self):
+        return f'{self.city}, ({self.street})'
+
 
 class Plot(models.Model):
-    plot_price = models.PositiveSmallIntegerField(verbose_name='Plot price', default=0, null=True, blank=False)
-    total_area = models.DecimalField(verbose_name='Total area', max_digits=7, decimal_places=2, null=True, blank=False)
+    BUILDIN_PLOT = 0
+    AGRICULTURAL_PLOT = 1
+    INVESTMENT_PLOT = 2
+
+    PLOT_TYPES = (
+        (BUILDIN_PLOT, 'Building'),
+        (AGRICULTURAL_PLOT, 'Agricultural'),
+        (INVESTMENT_PLOT, 'External'),
+    )
+
+    plot_type = models.IntegerField(verbose_name='Plot type', choices=PLOT_TYPES, null=False, blank=False)
+    plot_price = models.PositiveSmallIntegerField(verbose_name='Plot price', default=0, null=True, blank=True)
+    total_area = models.DecimalField(verbose_name='Total area', max_digits=7, decimal_places=2, null=True,
+                                     blank=True)
     address = models.OneToOneField(Address, on_delete=models.CASCADE, verbose_name='Address', null=True, blank=False)
+
+    def __str__(self):
+        return f'{self.get_plot_type_display()}, ({self.address.city}, {self.address.street})'
 
 
 class Realestate(models.Model):
@@ -20,9 +38,17 @@ class Realestate(models.Model):
     REALESTATE_SINGLE_FAMILY = 1
 
     REALESTATE_TYPES = (
-        (REALESTATE_MULTI_FAMILY, 'Multi-family'),
-        (REALESTATE_SINGLE_FAMILY, 'Single-family')
+        (REALESTATE_MULTI_FAMILY, 'Multi-family house'),
+        (REALESTATE_SINGLE_FAMILY, 'Single-family house')
     )
+
+    KITCHEN_OPEN = 0
+    KITCHEN_CLOSED = 1
+
+    KITCHEN = [
+        (KITCHEN_OPEN, 'Kitchen open'),
+        (KITCHEN_CLOSED, 'Kitchen closed')
+    ]
 
     plot = models.ForeignKey(Plot, on_delete=models.CASCADE, verbose_name='Plot', null=True, blank=False)
     realestate_type = models.IntegerField(verbose_name='Realestete type', choices=REALESTATE_TYPES, null=True,
@@ -32,13 +58,19 @@ class Realestate(models.Model):
     realestate_area = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Realestete area', null=True,
                                           blank=False)
     number_floors = models.PositiveSmallIntegerField(default=0, verbose_name='Number floors', null=True, blank=False)
+    rooms = models.PositiveSmallIntegerField(default=0, verbose_name='Rooms', null=True, blank=True)
+    kitchen = models.IntegerField(verbose_name='Kitchen', choices=KITCHEN, null=True, blank=True)
+    bathroom = models.PositiveSmallIntegerField(default=0, verbose_name='Bathroom', null=True, blank=True)
+    year_built = models.DateField(auto_now_add=False, verbose_name='Year built', null=True, blank=True)
+    description = models.TextField(default='', verbose_name='Description', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.get_realestate_type_display()}, ({self.plot.address.city}, {self.plot.address.street})'
 
 
 class Flat(models.Model):
     realestate = models.ForeignKey(Realestate, on_delete=models.CASCADE, verbose_name='Realestate', null=True,
                                    blank=False)
-    flat_price = models.PositiveSmallIntegerField(default=0, verbose_name='Flat price', null=True, blank=False)
-    flat_area = models.DecimalField(max_digits=5, verbose_name='Flat area', decimal_places=2, null=True, blank=False)
     floor_number = models.PositiveSmallIntegerField(default=0, verbose_name='Floor number', null=True, blank=True)
 
 
@@ -57,7 +89,8 @@ class Garage(models.Model):
         (BELONGING_GARAGE, 'Belonging')
     )
 
-    realestate = models.ForeignKey(Realestate, on_delete=models.CASCADE, null=True, blank=False)
+    realestate = models.ForeignKey(Realestate, on_delete=models.CASCADE, verbose_name='Realestate', null=True,
+                                   blank=False)
     garage_type = models.IntegerField(verbose_name='Garage type', choices=GARAGE_TYPES, null=True, blank=False)
     garage_price = models.PositiveSmallIntegerField(default=0, verbose_name='Garage price', null=True, blank=False)
 
