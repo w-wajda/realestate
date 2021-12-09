fb3b610098b2e177e021d14a78f8e731323d3c22
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2021-12-09 21:28:42 +0100

    update

--- realestates/models.py
+++ realestates/models.py
@@ -10,20 +10,51 @@ class Address(models.Model):
 
 
 class Plot(models.Model):
+    plot_price = models.PositiveSmallIntegerField(default=0, null=True, blank=False)
     total_area = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=False)
-    price = models.PositiveSmallIntegerField(default=0, null=True, blank=False)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True, blank=True)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, verbose_name='Address', null=True, blank=True)
 
 
 class Realestate(models.Model):
-    plot = models.ForeignKey(Plot, on_delete=models.CASCADE, null=True, blank=False, )
+    REALESTATE_MULTI_FAMILY = 0
+    REALESTATE_SINGLE_FAMILY = 1
+
+    REALESTATE_TYPES = (
+        (REALESTATE_MULTI_FAMILY, 'Multi-family'),
+        (REALESTATE_SINGLE_FAMILY, 'Single-family')
+    )
+
+    plot = models.ForeignKey(Plot, on_delete=models.CASCADE, verbose_name='Plot', null=True, blank=False)
+    realestate_type = models.IntegerField(verbose_name='Realestete type', choices=REALESTATE_TYPES, null=True,
+                                          blank=False)
+    realestate_price = models.PositiveSmallIntegerField(default=0, null=True, blank=False)
+    realestate_area = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=False)
+    number_floors = models.PositiveSmallIntegerField(default=0, null=True, blank=False)
 
 
 class Flat(models.Model):
     realestate = models.ForeignKey(Realestate, on_delete=models.CASCADE, null=True, blank=False)
+    flat_price = models.PositiveSmallIntegerField(default=0, null=True, blank=False)
+    flat_area = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=False)
+    floor_number = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
 
 
 class Garage(models.Model):
+    NO_GARAGE = 0
+    UNDERGROUND_GARAGE = 1
+    EXTERNAL_PARKING_SPACE = 2
+    DETACHED_GARAGE = 3
+
+    GARAGE_TYPES = (
+        (NO_GARAGE, 'No'),
+        (UNDERGROUND_GARAGE, 'Underground'),
+        (EXTERNAL_PARKING_SPACE, 'External'),
+        (DETACHED_GARAGE, 'Detached'),
+    )
+
     realestate = models.ForeignKey(Realestate, on_delete=models.CASCADE, null=True, blank=False)
+    garage_price = models.PositiveSmallIntegerField(default=0, null=True, blank=False)
+    garage_type = models.IntegerField(verbose_name='Garage type', choices=GARAGE_TYPES, null=True, blank=False)
+
 
 
