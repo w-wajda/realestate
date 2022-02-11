583a3d6efb70ad76806822bbc5e9c26c8dc4aad3
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2022-02-11 17:53:24 +0100

    add new router.register

--- base/urls_api.py
+++ base/urls_api.py
@@ -4,6 +4,13 @@ from realestates import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'clients', views.ClientViewSet)
+router.register(r'addresses', views.AddressViewSet)
+router.register(r'plots', views.PlotViewSet)
+router.register(r'realestates', views.RealestateViewSet)
+router.register(r'flats', views.FlatViewSet)
+router.register(r'garages', views.GarageViewSet)
+router.register(r'offers', views.OfferViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
