9de7f07cc8b84c6d3a1b0d2559c3a508faf86860
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2022-02-11 19:27:34 +0100

    add method create to UserSerializer

--- realestates/serializers.py
+++ realestates/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from rest_framework.validators import UniqueValidator
 
 from realestates.models import (
     Client,
@@ -16,7 +18,19 @@ from realestates.models import (
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email']
+        fields = ['id', 'username', 'first_name', 'last_name', 'password', 'email']
+
+        extra_kwargs = {
+            'password': {'required': True, 'write_only': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'email': {'required': True, 'validators': [UniqueValidator(queryset=User.objects.all())]}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
 
 
 class ClientSerializer(serializers.ModelSerializer):
