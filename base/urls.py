dfd75ec7afed4292cdc26c7ab5429d2315294a61
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2022-02-11 19:28:18 +0100

    setting path with api-token-auth

--- base/urls.py
+++ base/urls.py
@@ -4,10 +4,14 @@ from django.urls import (
     include
 )
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 from base import urls_api
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(urls_api)),
+    path('api-token-auth/', obtain_auth_token)
 ]
+
 
 
