60d13888b27bb25bb840ee842bb804241dc3cd45
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2022-02-11 20:13:24 +0100

    create view AddressAdmin and ClientAdmin in admin

--- realestates/admin.py
+++ realestates/admin.py
@@ -9,10 +9,56 @@ from realestates.models import (
     Offer
 )
 
-admin.site.register(Address)
-admin.site.register(Client),
-admin.site.register(Plot)
-admin.site.register(Realestate)
-admin.site.register(Flat)
-admin.site.register(Garage)
-admin.site.register(Offer)
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('city', 'street', 'street_number')
+    list_filter = ('city', )
+    search_fields = ['city', 'street']
+    fieldsets = (
+        ('Address data', {
+            'fields': (('street', 'street_number'), ('zip_code', 'city')),
+        }),
+    )
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    list_display = ('name', 'surname', 'email', 'mobile_number')
+    list_filter = ('name', 'surname',)
+    search_fields = ['name', 'surname', 'email', 'mobile_number']
+    fieldsets = (
+        ('Client data', {
+            'fields': (('name', 'surname'), )
+        }),
+        ('Contact details', {
+            'fields': (('email', 'mobile_number'), )
+        })
+    )
+
+
+@admin.register(Plot)
+class PlotAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+
+
+@admin.register(Realestate)
+class RealestateAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+
+
+@admin.register(Flat)
+class FlatAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+
+
+@admin.register(Garage)
+class GarageAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+
+
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+
