ce254e40076434e1e14f6c564172d17876868522
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2022-02-11 21:20:48 +0100

    add method client_data_together for ClientAdmin

--- realestates/admin.py
+++ realestates/admin.py
@@ -12,7 +12,6 @@ from realestates.models import (
 
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
-    list_display = ('city', 'street', 'street_number')
     list_filter = ('city', )
     search_fields = ['city', 'street']
     fieldsets = (
@@ -25,7 +24,7 @@ class AddressAdmin(admin.ModelAdmin):
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     empty_value_display = '-empty-'
-    list_display = ('name', 'surname', 'email', 'mobile_number')
+    list_display = ('client_data_together', 'email', 'mobile_number')
     list_filter = ('name', 'surname',)
     search_fields = ['name', 'surname', 'email', 'mobile_number']
     fieldsets = (
@@ -37,10 +36,15 @@ class ClientAdmin(admin.ModelAdmin):
         })
     )
 
+    @admin.display(description='Name and Surname')
+    def client_data_together(self, obj):
+        return "%s %s" % (obj.name, obj.surname)
+
 
 @admin.register(Plot)
 class PlotAdmin(admin.ModelAdmin):
     empty_value_display = '-empty-'
+
 
 
 @admin.register(Realestate)
